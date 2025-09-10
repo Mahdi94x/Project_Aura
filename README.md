ba8544abf46c718dae69ae0535db8aa44f92ed2b
Mahdi Mahmoud <mahdim.mahmoud@gmail.com>
2025-09-11 00:08:54 +0300

    Project Creation & AuraCharacterBase Class
    - Adjusting .gitattributes file
    - Adjusted Jetbrain Rider to be the main IDE for the project
    - Created C++ class AuraCharacterBase to hold the common logic for the main character and the enemies

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project_Aura
+
+Developed with Unreal Engine 5.6.1
---
+++ Source/Project_Aura/Public/Characters/AuraCharacterBase.h
@@ -0,0 +1,29 @@
+// Project by Mahdi94x based on Stephen Ulibarri's create a multiplayer RPG with Unreal Engine's Gameplay Ability System (GAS) Course.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AuraCharacterBase.generated.h"
+
+UCLASS()
+class PROJECT_AURA_API AAuraCharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AAuraCharacterBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
