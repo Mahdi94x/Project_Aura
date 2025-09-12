649784176fab56708c41c8306ccbd41619db803b
Mahdi Mahmoud <mahdim.mahmoud@gmail.com>
2025-09-12 21:05:14 +0300

    Characters Hierarchy
    - Making AuraCharacterBase class abstract to prevent dragging into the level and design it as a parent class.
    - Created AuraCharacter class and AuraEnemy class based on the AuraCharacterBase
    - Added a USkeltelMeshCompoent TObjectPtr<> in the AuraCharacterBase and created it in the constructor
    - Created BP_AuraCharacter and BP_Goblin_Spear based on AuraCharacter class and AuraEnemy class respectively and adjusted the weapon socket, tested blueprints in the level.

---
+++ Source/Project_Aura/Public/Characters/AuraCharacter.h
@@ -0,0 +1,17 @@
+// Project by Mahdi94x based on Stephen Ulibarri's create a multiplayer RPG with Unreal Engine's Gameplay Ability System (GAS) Course.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/AuraCharacterBase.h"
+#include "AuraCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_AURA_API AAuraCharacter : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
