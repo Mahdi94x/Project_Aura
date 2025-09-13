37ec52b40a50c008ad7761f9080b13e15f3105d9
Mahdi Mahmoud <mahdim.mahmoud@gmail.com>
2025-09-13 22:07:51 +0300

    Enhanced Input Component and Game Mode
    - In AuraPlayerComponent class, added a UInputAction and callback function AuraMove() to implement the movement logic of the main character using FInputActionValue struct and getting the controlled pawn and then using AddMovementInput
    - Created BP_AuraPlayerController and setup the AuraMoveAction and AuraMappingContext
    - Created AuraGameModeBase C++ class and a blueprint to tie the EnhancedInputSystem to the main character, overridden the GameMode of the project to the custom created one
    - Added a constructor in AuraCharacter C++ class to adjust the parameters of the MovementComponent like bOrientToMovement and RotationRate
    - Updated ABP_AuraCharacter to separate the Idle state from the IdleWalkRun state as the animations were jittery by creating a boolean ShouldAuraMove

--- Source/Project_Aura/Private/PlayerController/AuraPlayerController.cpp
+++ Source/Project_Aura/Private/PlayerController/AuraPlayerController.cpp
@@ -27,3 +27,30 @@ void AAuraPlayerController::BeginPlay()
 	InputModeData.SetHideCursorDuringCapture(true);
 	SetInputMode(InputModeData);
 }
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent))
+	{
+		EnhancedInputComponent->BindAction(AuraMoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::AuraMove);
+	}
+
+}
+
+void AAuraPlayerController::AuraMove(const FInputActionValue& InputActionValue)
+{
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation(0.f,Rotation.Yaw,0.f);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	if (APawn* ControlledPawn = GetPawn())
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection,InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection,InputAxisVector.X);
+	}
+}
