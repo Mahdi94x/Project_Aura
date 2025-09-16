aae848237fdb3bcb0329b163c9541108550e09ec
Mahdi Mahmoud <mahdim.mahmoud@gmail.com>
2025-09-16 04:38:00 +0300

    Integrating GAS Into The Project
    - Created AuraPlayerState C++ class based on APlayerState and BP_AuraPlayerState based on the created C++ class to start integrating GAS system in the project
    - Assigned BP_AuraPlayerState to BP_AuraGameModeBase
    - Created AuraAbilitySystemComponent C++ class
    - Created AuraAttributeSet C++ class
    - Updated PrivateDependencyModuleNames in the C# build file to include modules related to GAS

---
+++ Source/Project_Aura/Public/AbilitySystem/AuraAbilitySystemComponent.h
@@ -0,0 +1,17 @@
+// Project by Mahdi94x based on Stephen Ulibarri's create a multiplayer RPG with Unreal Engine's Gameplay Ability System (GAS) Course.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "AuraAbilitySystemComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_AURA_API UAuraAbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Project_Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -0,0 +1,17 @@
+// Project by Mahdi94x based on Stephen Ulibarri's create a multiplayer RPG with Unreal Engine's Gameplay Ability System (GAS) Course.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AuraAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_AURA_API UAuraAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
