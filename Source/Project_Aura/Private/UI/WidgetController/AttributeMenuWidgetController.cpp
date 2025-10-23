a39bb29e76c3aee3c355424a7dce845dbf36e13f
Mahdi Mahmoud <mahdim.mahmoud@gmail.com>
2025-10-23 20:44:04 +0300

    AttributeMenu Broadcasting Initial Values
    - To automate the process of broadcasting the inital values from the AttributeMenu widgetController to the UI, TMap was created in AuraAttributeSet to bind a gameplay tag to a function pointer to get the attribute
    - In void UAttributeMenuWidgetController::BroadcastInitialValues(), an algorithm is created to loop through the map and broadcast the info struct after setting the attribute value using Pair.Value().GetNumericValue(AttributeSet);

--- Source/Project_Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/Project_Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -3,21 +3,20 @@
 
 #include "UI/WidgetController/AttributeMenuWidgetController.h"
 #include "AbilitySystem/AuraAttributeSet.h"
-#include "AuraGameplayTags.h"
 
 void UAttributeMenuWidgetController::BroadcastInitialValues()
 {
 	UAuraAttributeSet* AS = CastChecked<UAuraAttributeSet>(AttributeSet);
 	check(AttributeInfo);
 	
-	FAuraAttributeInfo Info = AttributeInfo->FindAttributeInfoByTag(FAuraGameplayTags::Get().Attributes_Primary_Strength);
-	Info.AttributeValue = AS->GetStrength();
-	AttributeInfoDelegate.Broadcast(Info);
+	for (auto& Pair : AS->TagsToAttributes)
+	{
+		FAuraAttributeInfo Info = AttributeInfo->FindAttributeInfoByTag(Pair.Key);
+		Info.AttributeValue = Pair.Value().GetNumericValue(AS);
+		AttributeInfoDelegate.Broadcast(Info);
+	}
 }
 
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 {
-	
-	
-	
 }
