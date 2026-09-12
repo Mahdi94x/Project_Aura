602cf989f4fac0d2b5fb5c7c73cea7cd2e863c99
Mahdi Mahmoud <mahdim.mahmoud@gmail.com>
2026-09-12 16:22:29 +0300

    Summon Ability - Async Spawn Times & Particle Effect
    - Updated GA_SummonAbility, Added asynchronous spawn-location processing across multiple frames after spawning a particle effect to indicate that an enemy is about to be spawned in that location (Enhancing visual feedback)
    - Added spawn location index with a delayed loop (0.2) to process one location at a time
    - Shuffled spawn locations for more randomized summon positioning
    - Upgraded the algorithm in Aura_SummonAbility::GetSpawnLocation(), Added downward line traces to ensure spawn locations align with the ground, including slopes and ramps

--- Source/Project_Aura/Private/AbilitySystem/Abilities/Aura_SummonAbility.cpp
+++ Source/Project_Aura/Private/AbilitySystem/Abilities/Aura_SummonAbility.cpp
@@ -1,7 +1,7 @@
 // Project by Mahdi94x based on Stephen Ulibarri's create a multiplayer RPG with Unreal Engine's Gameplay Ability System (GAS) Course.
 
 #include "AbilitySystem/Abilities/Aura_SummonAbility.h"
-#include "Kismet/KismetSystemLibrary.h"
+//#include "Kismet/KismetSystemLibrary.h"
 
 TArray<FVector> UAura_SummonAbility::GetSpawnLocations()
 {
@@ -24,14 +24,26 @@ TArray<FVector> UAura_SummonAbility::GetSpawnLocations()
 	for (int32 i = 0; i < NumOfMinions; i++)
 	{
 		const FVector Direction = LeftOfSpread.RotateAngleAxis(DeltaSpread * i, FVector::UpVector); /*Unit-Vector*/
-		const FVector ChosenSpawnLocation = Location + Direction * FMath::RandRange(MinSpawnDistance, MaxSpawnDistance); /*Scaled-Vector*/
+		FVector ChosenSpawnLocation = Location + Direction * FMath::RandRange(MinSpawnDistance, MaxSpawnDistance); /*Scaled-Vector*/
+		
+		FHitResult Hit;
+		GetWorld()->LineTraceSingleByChannel(Hit,
+			ChosenSpawnLocation + FVector(0.f,0.f,400.f),
+			ChosenSpawnLocation - FVector(0.f,0.f,400.f),
+			ECC_Visibility);
+		
+		if (Hit.bBlockingHit)
+		{
+			ChosenSpawnLocation = Hit.ImpactPoint;
+		}
+		
 		SpawnLocations.AddUnique(ChosenSpawnLocation);
 		
-		UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(),
+		/*UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(),
 		Location, Location + (Direction * MaxSpawnDistance), 4.f, FLinearColor::Green, 3.f);
 		DrawDebugSphere(GetWorld(), Location + (Direction * MinSpawnDistance), 8.f,12,FColor::Red,false,3.f);
 		DrawDebugSphere(GetWorld(), Location + (Direction * MaxSpawnDistance), 8.f,12,FColor::Red,false,3.f);
-		DrawDebugSphere(GetWorld(), ChosenSpawnLocation, 15.f,12,FColor::Cyan,false,3.f);
+		DrawDebugSphere(GetWorld(), ChosenSpawnLocation, 15.f,12,FColor::Cyan,false,3.f);*/
 	}
 	
 	return SpawnLocations;
